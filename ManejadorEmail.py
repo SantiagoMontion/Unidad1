403bdd8d4a10f32f8d68da6f2efd6e5f38eb058a
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-07 14:59:23 -0300

    Add files via upload

---
+++ ManejadorEmail.py
@@ -0,0 +1,36 @@
+from ClaseEmail import Email
+import csv
+
+class ManejadorEmail:
+    __lista=[]
+
+    def __init__(self):
+        self.__lista=[]
+
+
+    def CargarLista(self):
+        archivo=open("C:/Users/Montion/Desktop/Facultad/POO/Unidad2/Repositorio/Ejercicio1/Listado.csv")
+        reader=csv.reader(archivo,delimiter=',')
+        
+        for fila in reader:
+            for j in range(10):
+                f=str(fila[j])
+                separado=f.split("@")
+                id=separado[0]
+                sep2=separado[1].split(".")
+                dominio=sep2[0]
+                tipo=sep2[1]
+
+                E=Email(id,dominio,tipo)
+                self.__lista.append(E)
+        archivo.close()
+
+        
+    def BuscarDominio(self,dom):
+        cont=0
+        print(len(self.__lista))
+        for i in range(len(self.__lista)):
+            if str(self.__lista[i].getdominio())==str(dom):
+                cont+=1
+
+        print("La cantidad de cuentas con el dominio {} es de: {}".format(dom,cont))
