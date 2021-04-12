da3ae55e878b70b918fb48ea5f71653c99d85cf8
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-12 18:54:35 -0300

    Update ManejadorEmail.py

--- ManejadorEmail.py
+++ ManejadorEmail.py
@@ -1,5 +1,6 @@
 from ClaseEmail import Email
 import csv
+import re
 
 class ManejadorEmail:
     __lista=[]
@@ -9,28 +10,33 @@ class ManejadorEmail:
 
 
     def CargarLista(self):
-        archivo=open("Listado.csv")
+        archivo=open("C:/Users/Montion/Desktop/Facultad/POO/Unidad2/Repositorio/Ejercicio1/Listado.csv")
         reader=csv.reader(archivo,delimiter=',')
-        
         for fila in reader:
             for j in range(10):
-                f=str(fila[j])
-                separado=f.split("@")
-                id=separado[0]
-                sep2=separado[1].split(".")
-                dominio=sep2[0]
-                tipo=sep2[1]
-
-                E=Email(id,dominio,tipo)
-                self.__lista.append(E)
+                correo=str(fila[j])
+                if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$',correo.lower()):
+                    f=str(fila[j])
+                    separado=f.split("@")
+                    id=separado[0]
+                    sep2=separado[1].split(".")
+                    dominio=sep2[0]
+                    tipo=sep2[1]
+                    E=Email(id,dominio,tipo)
+                    self.__lista.append(E)
+                    
+                else:
+                    print ("Correo incorrecto")
         archivo.close()
+        
 
         
     def BuscarDominio(self,dom):
         cont=0
-        print(len(self.__lista))
+        self.CargarLista()
         for i in range(len(self.__lista)):
             if str(self.__lista[i].getdominio())==str(dom):
                 cont+=1
 
         print("La cantidad de cuentas con el dominio {} es de: {}".format(dom,cont))
+
