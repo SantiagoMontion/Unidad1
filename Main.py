abc1080ef6b57b4a67461b4dbd89c7a3ff24899b
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-13 18:30:20 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,34 +1,20 @@
-from ManejadorViajeros import Manejador
+import os
+from Menu import Menu
 
-def menu():
-    print("\na- Consultar Cantidad de Millas.\n")
-    print("b- Acumular Millas.\n")
-    print("c- Canjear Millas.\n")
-    op=input("Seleccione una opcion: ")
-    return op
 
 if __name__=='__main__':
-    M=Manejador()
-    M.CargarLista()
-
+    Menu=Menu()
     num=int(input("Ingrese un numero de viajero frecuente "))
 
     while num!=0:
-        opcion=str(menu()).lower()
         
-        if opcion=='a':
-            print("La Cantidad de Millas es: {}".format(M.ConsultaMillas(num)))
-
-        elif opcion=='b':
-            millas=int(input("Ingrese La cantidad de millas para acumular "))
-            M.Acumular(num,millas)
-
-        elif opcion =='c':
-            millas=int(input("Ingrese La cantidad de millas para canjear "))
-            M.Canjear(num,millas)
+        print("\na- Consultar Cantidad de Millas.\n")
+        print("b- Acumular Millas.\n")
+        print("c- Canjear Millas.\n")
+        op=str(input("Seleccione una opcion: ")).lower()
+        os.system('cls')
+        Menu.opcion(op,num)
         
-        else:
-            print("Opcion inocrrecta\n")
 
         
         num=int(input("\nIngrese otro numero o 0 para finalizar "))
