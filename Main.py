d7b0392a784612e51f6260307c71a474c6149380
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-26 18:55:23 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,98 +1,73 @@
+from ClaseFechaHora import FechaHora
 
-class FechaHora:
-    __dia=0
-    __mes=0
-    __anio=0
-    __hora=0
-    __min=0
-    __seg=0
+def test():
+    t1=FechaHora(29,2,2012,15,41,0)
 
-
-    def __init__(self,dia=1,mes=1,anio=2020,hora=0,min=0,seg=0):
-        try:
-            self.__dia=int(dia)
-            self.__mes=int(mes)
-            self.__anio=int(anio)
-            self.__hora=int(hora)
-            self.__min=int(min)
-            self.__seg=int(seg)
-            self.check()
-        except:
-            print("Datos Incorrectos\n")
+    t1.Mostrar()
+    input()
+    t2=FechaHora(28,2,2000,23,40,0)
+    t2.AdelantarHora(3)
+    t2.Mostrar()
 
 
-    def Mostrar(self):
-        print("\nFECHA {}/{}/{}\nHoras {}hs Minturos {}m Segundos {}s".format(self.__dia,self.__mes,self.__anio,self.__hora,self.__min,self.__seg))
+if __name__ == '__main__':
+
+    d=int(input("Ingrese Dia: "))
+
+    mes=int(input("Ingrese Mes: "))
+
+    a=int(input("Ingrese Año: "))
+
+    h=int(input("Ingrese Hora: "))
+
+    m=int(input("Ingrese Minutos: "))
+
+    s=int(input("Ingrese Segundos: "))
+
+    r = FechaHora () #  inicilizar día, mes, año con 1/1/2020, y hora, minutos y 
+
+                              #  segundos con 0h, 0m, 0s.
+
+    r1 = FechaHora(d,mes,a); # inicializar con 0h 0m 0s
+
+    r2= FechaHora(d,mes,a,h,m,s)
+
+    r.Mostrar()
+
+    r1.Mostrar()
+
+    r2.Mostrar()
+
+    input()
+
+    r.PonerEnHora(5) # solamente la hora
+
+    r.Mostrar()
+
+    input()
+
+    r2.PonerEnHora(13,30) #hora y minutos
+
+    r2.Mostrar()
+
+    input()
+
+    r.PonerEnHora(14, 30, 25) #hora, minutos y segundos
+
+    r.Mostrar()
+
+    input()
+
+    r.AdelantarHora(3) #sumar 3 horas a la hora actual
+
+    r.Mostrar()
+
+    input()
+
+    r1.AdelantarHora(1,15) #sumar 1 hora y 15 minutos a la hora actual
+    
+    r1.Mostrar()
 
 
-    def PonerEnHora(self,Hora,min=0,seg=0):
-        if type(Hora)==int and type(min)==int and type(seg)==int:
-            self.__hora=Hora
-            self.__min=min
-            self.__seg=seg
-
-        else:
-            print("Datos Incorrectos\n")
-
-
-    def AdelantarHora(self,hora,min=0):
-        if type(hora)==int and type(min)==int:
-            self.__hora+=int(hora)
-            self.__min+=int(min)
-            self.check()
-           
-        else:
-            print("Datos Incorrectos\n")
-
-
-
-    def check(self):
-
-        if self.__seg>=60:
-            self.__min+=1
-            self.__seg=0
-        
-        if self.__min>=60:
-            self.__hora+=1
-            self.__min=0
-
-        if self.__hora>=24:
-            self.__dia+=1
-            self.__hora=0
-
-
-
-#divisible por 4, y si es divisible por 100, debe ser divisible por 400
-        if self.__anio%4==0 and self.__anio%100!=0:
-            #Año bisiesto
-            
-            if self.__mes==2 and self.__dia>29:
-                print("ACA")
-                print(self.__dia)
-                self.__dia=1
-                self.__mes+=1
-        elif self.__anio%100==0 and self.__anio%400 ==0:
-            #Año bisiesto
-            
-            if self.__mes==2 and self.__dia>29:
-                print("ACA NO")
-                self.__dia=1
-                self.__mes+=1
-
-        else:
-            if self.__mes == 2 and self.__dia>28:
-                self.__mes+=1
-                self.__dia=1
-                
-            
-            if (self.__mes == 4 or self.__mes==6 or self.__mes==9 or self.__mes==11) and (self.__dia>30):
-                self.__dia=1
-                self.__mes+=1
-       
-            
-            elif (self.__mes==1 or self.__mes==3 or self.__mes==5 or self.__mes==7 or self.__mes==8 or self.__mes==10 or self.__mes==12) and (self.__dia>31):
-                self.__dia=1
-                self.__mes+=1
-                if self.__mes>12:
-                    self.__mes=1
-                    self.__anio+=1
+    print("-----Test-----")
+    test()
