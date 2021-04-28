ad8a36525bc5940f91424af917feb47b3d468bac
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-27 21:59:38 -0300

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -24,15 +24,21 @@ class Menu:
 
 
     def opcion1(self):
+        print('\n---Opcion1---')
         num=int(input("Ingrese el id del camion: "))
         k=self.__C.Obtenerkilosdescargados(num-1)
-        print("La cantidad de kilos descargados fue: ",k)
+        if k>=0:
+            print("La cantidad de kilos descargados fue: ",k)
+        else:
+            print("La cantidad de kilos descargados fue: 0")
 
     def opcion2(self):
+        print('\n---Opcion2---')
         dia=int(input("Ingrese el dia: "))
         self.__C.Mostrarlistado(dia-1)
 
     def opcion3(self):
+        print('\n---Opcion3---')
         num=int(input("Ingrese el id del camion: "))
         dia=int(input("Ingrese el dia: "))
         peso=float(input("Ingrese el peso total: "))
