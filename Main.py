57b57f99e094b9de34f689aea465f632feff4188
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-27 21:58:35 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
 from Menu import Menu
 import os
 
+def test(o1,o2,o3,Menu):
+    Menu.opcion(o1)
+    Menu.opcion(o2)
+    Menu.opcion(o3)
 
 
 if __name__=='__main__':
@@ -10,6 +14,7 @@ if __name__=='__main__':
     os.system('cls')
 
     while enter=='':
+
         
         
         print("1. Mostrar la cantidad total de kilos descargados.\n")
@@ -22,4 +27,7 @@ if __name__=='__main__':
         enter=input("Presione enter para abrir el Menu. (Cualquier otra tecla para salir)")
 
 
-    
+    test('1','2','3',Menu)
+
+
+   
