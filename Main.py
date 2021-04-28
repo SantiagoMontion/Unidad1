49c56c09efd32d71e7855ea435cc34d038c52b97
SantiagoMontion <82060780+SantiagoMontion@users.noreply.github.com>
2021-04-27 23:48:02 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,25 @@ from ManejadorIntegrantes import ManejadorIntegrantes
 from ManejadorProyectos import ManejadorProyecto
 
 
+def test():
+    print("-------TEST-------")
+    MItest=ManejadorIntegrantes()
+    MPtest=ManejadorProyecto()
+    MItest.CargarTest()
+    MPtest.CargarTest()
+    print("\n-------Reglas de negocio------")
+    MPtest.CalcularPuntos(MItest)
+    input("\nEnter para continuar")
+
+    print("-------Listado de puntos--------")
+
+
+    MPtest.MostrarPuntos()
+
+
+
+
+
 if __name__ == '__main__':
     MI=ManejadorIntegrantes()
     MP=ManejadorProyecto()
@@ -17,3 +36,6 @@ if __name__ == '__main__':
     print("-------Listado de puntos--------")
 
     MP.MostrarPuntos()
+
+
+    test()
